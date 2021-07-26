eeea89e4bd787912d5229029b5cedb74cbfac9f3
gywndi <gywndi@gmail.com>
2021-07-26 12:10:52 +0900

    add sample code

--- src/main/java/net/gywn/SynbagServer.java
+++ src/main/java/net/gywn/SynbagServer.java
@@ -1,6 +1,7 @@
 package net.gywn;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.sql.DataSource;
 
@@ -13,6 +14,30 @@ public class SynbagServer {
 
         DataSource ds = synbagConfig.getShardingDatasource();
         Connection conn = ds.getConnection();
+
+		PreparedStatement pstmt = null;
+        pstmt = conn.prepareStatement("delete from user");
+        int rows = pstmt.executeUpdate();
+        // System.out.println(rows+" deleted");
+        pstmt.close();
+
+        pstmt = conn.prepareStatement("insert ignore into user(userid, name, dttm)values (?,?, now(6))");
+		for (int i = 0; i < 100; i++) {
+			pstmt.setString(1, "abcde" + i);
+			pstmt.setString(2, "abcde-name" + i);
+			pstmt.executeUpdate();
+			pstmt.clearParameters();
+		}
+		pstmt.close();
+
+		pstmt = conn.prepareStatement("insert ignore into config (ver, info) values (?,?)");
+		for (int i = 0; i < 10; i++) {
+			pstmt.setString(1, "0." + i);
+			pstmt.setString(2, "version:" + i);
+			pstmt.executeUpdate();
+			pstmt.clearParameters();
+		}
+		pstmt.close();
         conn.close();
     }
 }
